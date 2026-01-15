b72f5f53f82900c1805f1324fc98c2bd5bd23f0f
M3ln1k <whynot-2@yandex.ru>
2026-01-15 12:51:58 +0300

    new test page

---
+++ test.php
@@ -0,0 +1,6 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("title", "test");
+$APPLICATION->SetPageProperty("keywords", "test");
+$APPLICATION->SetTitle("test");
+?>tets<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
